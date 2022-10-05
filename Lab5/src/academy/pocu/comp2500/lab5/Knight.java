d4a2941e275bdfd40ab2ddf00167b0d579e704e5
Joo-hyun-Kang <autoba9687@gmail.com>
2022-10-05 16:35:55 +0900

    feat: implement Knight and pet

---
+++ Lab5/src/academy/pocu/comp2500/lab5/Knight.java
@@ -0,0 +1,22 @@
+package academy.pocu.comp2500.lab5;
+
+public class Knight extends Barbarian {
+    private Pet pet;
+
+    public Knight(String name, int hp, int attack, int defense) {
+        super(name, hp, attack, defense);
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+
+    public void attackTogether(Barbarian target) {
+        if (super.name.equals(target.name) || !isAlive() || pet == null) {
+            return ;
+        }
+
+        double damage = (int) ((super.attack + pet.attack - target.defense) / 2.0);
+        damage = Math.max(damage, 1);
+    }
+}
